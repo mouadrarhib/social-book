c91e0009b1188ea8ca8a1f9ca68de249b1206e59
MOUAD RARHIB <mouad159mouad@gmail.com>
2025-02-23 12:31:16 +0000

    Fix the UI

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import {ActivateAccountComponent} from './pages/activate-account/activate-accoun
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     component: LoginComponent
   },
--- book-network-ui/src/app/modules/book/components/menu/menu.component.ts
+++ book-network-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+    username = 'mouad';
     ngOnInit(): void {
       const linkColor = document.querySelectorAll('.nav-link');
       linkColor.forEach(link => {
